50d1a68cbddfdf4731afadf2867e8bcb0fce3ae3
chakeradel <chakeradel30@gmail.com>
2024-10-08 09:24:19 +0100

    home screen added + standard components: cards,section and customButton

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,13 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+// App.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './app/screens/HomeScreen';
+import EventsScreen from './app/screens/EventsScreen';
+import ServicesScreen from './app/screens/ServicesScreen';
+import AccountScreen from './app/screens/AccountScreen';
 
-const App = () => {
+const Tab = createBottomTabNavigator();
+
+export default function App() {
   return (
-    <View style={{ flex: 1 }}> {/* Use View instead of div */}
-      <StatusBar />
-   
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Accueil" component={HomeScreen} />
+        <Tab.Screen name="Événements" component={EventsScreen} />
+        <Tab.Screen name="Services" component={ServicesScreen} />
+        <Tab.Screen name="Mon Compte" component={AccountScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-};
-
-export default App;
+}
---
+++ my-app/app/screens/EventsScreen.tsx
@@ -0,0 +1,29 @@
+// screens/EventsScreen.tsx
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const EventsScreen: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Événements</Text>
+      <Button title="Créer un Événement" onPress={() => { /* Logique de création d'événement */ }} />
+      <Text>Liste des événements disponibles :</Text>
+      {/* Ajoutez ici la logique pour afficher la liste des événements */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
+
+export default EventsScreen;
