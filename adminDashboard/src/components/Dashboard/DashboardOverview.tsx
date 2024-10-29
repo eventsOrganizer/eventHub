e64c62f6d18ddf9b5744f6d4f6341ed2582bb03e
siwarameri31 <siwar.ameri31@gmail.com>
2024-10-29 11:30:09 +0100

    admin dashbord

---
+++ adminDashboard/src/components/Dashboard/DashboardOverview.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DashboardOverview: React.FC = () => {
+  return (
+    <div>
+      <h2>Dashboard Overview</h2>
+      {/* Add key metrics and graphs here */}
+    </div>
+  );
+};
+
+export default DashboardOverview;
---
+++ adminDashboard/src/components/Notifications/SystemAlerts.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SystemAlerts: React.FC = () => {
+  return (
+    <div>
+      <h2>System Alerts</h2>
+      {/* Display system alerts here */}
+    </div>
+  );
+};
+
+export default SystemAlerts;
